467545ccca8c0bc8b86cfb5adcfd3a38189d89aa
jessibreen <jessibreen@gmail.com>
2017-01-06 13:20:34 -0500

    here are the files

---
+++ php/flag.php
@@ -0,0 +1,8 @@
+<?php
+include '/var/www/cartodbProxy.php';
+//include '../../../../apis/cartodbProxy.php';
+//      ^CHANGE THIS TO THE PATH TO YOUR cartodbProxy.php
+$q = "update " .$_POST['table'] . " set flag = true where cartodb_id = " . $_POST['id'];
+$return = goProxy($q);
+echo $return;
+?>
---
+++ php/heart.php
@@ -0,0 +1,8 @@
+<?php
+include '/var/www/cartodbProxy.php';
+//include '../../../../apis/cartodbProxy.php';
+//      ^CHANGE THIS TO THE PATH TO YOUR cartodbProxy.php
+$q = "update " .$_POST['table'] . " set loved = loved " .$_POST['op'] . " where cartodb_id = " . $_POST['id'];
+$return = goProxy($q);
+echo $return;
+?>
